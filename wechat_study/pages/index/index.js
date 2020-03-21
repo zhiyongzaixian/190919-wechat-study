05ba0f0a9e7d435fea17beac87b32452f0f720b3
zhiyongzaixian <13852292775@163.com>
2020-03-21 16:14:04 +0800

    'index'

--- wechat_study/pages/index/index.js
+++ wechat_study/pages/index/index.js
@@ -3,17 +3,38 @@
 Page({
 
   /**
-   * 页面的初始数据
+   * 页面的初始数据 并没有实现数据劫持代理
    */
   data: {
-
+    msg: '初始化的数据'
   },
+
+  handleParent(){
+    console.log('parent');
+  },
+  handleChild() {
+    console.log('child');
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log('onLoad');
+    // React: this.setState() 1. 在自己的钩子函数(componentDidMount)中是异步的，2. 在非自身钩子函数中是同步的，如： 定时器的回调
+    // Vue: this.xxx = value; 数据劫持，代理
+    // 小程序中： this.setData({})
+    console.log(this.msg) // 
+    console.log(this.data.msg) // 
+    // this.data.msg = '修改之后的数据';
 
+    // 同步修改， 异步更新渲染
+    // why? 降低页面渲染的次数，提高性能
+    this.setData({
+      msg: '修改之后的数据'
+    })
+    console.log(this.data.msg);
   },
 
   /**
