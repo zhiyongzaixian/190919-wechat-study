fc58c36f156cfec6880b51ca3b246fb42139258a
zhiyongzaixian <13852292775@163.com>
2020-03-21 17:34:40 +0800

    'index'

--- wechat_study/pages/index/index.js
+++ wechat_study/pages/index/index.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据 并没有实现数据劫持代理
    */
   data: {
-    msg: '初始化的数据'
+    msg: '初始化的数据',
+    userInfo: {}
   },
 
   handleParent(){
@@ -15,32 +16,76 @@ Page({
   handleChild() {
     console.log('child');
   },
+  // 跳转至logs页面
+  toLogs(){
+    wx.redirectTo({
+      url: '/pages/logs/logs',
+    })
+  },
 
+
+  handleGetUserInfo(res){
+    if(res.detail.userInfo && !this.data.userInfo.nickName){
+      console.log('点击了。。。', res)
+      // console.log(res.detail.userInfo);
+      // 更新userInfo的状态数据
+      this.setData({
+        userInfo: res.detail.userInfo
+      })
+    }
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // debugger;
     console.log('onLoad');
+
+    // 获取用户信息
+    wx.getUserInfo({ // 必须是用户授权之后
+      success: (res) => {
+        console.log('获取用户信息成功： ', res.userInfo)
+        this.setData({
+          userInfo: res.userInfo
+        })
+      },
+      fail: () => {
+        console.log('获取用户信息失败')
+      }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
     // React: this.setState() 1. 在自己的钩子函数(componentDidMount)中是异步的，2. 在非自身钩子函数中是同步的，如： 定时器的回调
     // Vue: this.xxx = value; 数据劫持，代理
     // 小程序中： this.setData({})
-    console.log(this.msg) // 
-    console.log(this.data.msg) // 
-    // this.data.msg = '修改之后的数据';
+    // console.log(this.msg) // 
+    // console.log(this.data.msg) // 
+    // // this.data.msg = '修改之后的数据';
 
-    // 同步修改， 异步更新渲染
-    // why? 降低页面渲染的次数，提高性能
-    this.setData({
-      msg: '修改之后的数据'
-    })
-    console.log(this.data.msg);
+    // // 同步修改， 异步更新渲染
+    // // why? 降低页面渲染的次数，提高性能
+    // this.setData({
+    //   msg: '修改之后的数据'
+    // })
+    // console.log(this.data.msg);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log('onReady');
 
   },
 
@@ -48,6 +93,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log('onShow');
 
   },
 
