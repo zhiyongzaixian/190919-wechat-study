ab640fae75d561420e9e1591cb071e42f139406f
zhiyongzaixian <13852292775@163.com>
2020-03-23 15:41:27 +0800

    'xx'

---
+++ guigu_yinyue/components/NavHeader/NavHeader.js
@@ -0,0 +1,31 @@
+// components/NavHeader/NavHeader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String, // 规定传入的数据类型
+      value: '初始化数据' // 默认值
+    },
+    nav: {
+      type: String, // 规定传入的数据类型
+      value: '' // 默认值
+    }
+  }
+  ,
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
