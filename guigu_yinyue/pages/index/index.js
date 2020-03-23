47db3f9e124d4a1118d84ac5abf62c17c561b05c
zhiyongzaixian <13852292775@163.com>
2020-03-23 17:09:35 +0800

    'xx'

--- guigu_yinyue/pages/index/index.js
+++ guigu_yinyue/pages/index/index.js
@@ -10,13 +10,14 @@ Page({
   data: {
     bannersList: [], // 轮播图的数据
     recommendList: [], // 推荐歌曲的数据
+    topList: [], // 排行榜的数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
-    // 发送请求
+    // 获取轮播图的数据
     let bannerListData = await request('/banner', {type: 2})
     this.setData({
       bannersList: bannerListData.banners
@@ -27,6 +28,27 @@ Page({
     this.setData({
       recommendList: recommendListData.result
     })
+    
+    
+    // 获取排行榜的数据
+    let initIdxIds = [0, 1, 2, 3, 4];
+    let index = 0;
+    let resultArr = [];
+    let result; // 前端开发模式： 单例模式， 用一个标量保存多个对象，每次用完就指向新的对象，节省内存空间
+    while(index < initIdxIds.length){
+      result = await request(`/top/list?idx=${index++}`);
+      
+      let obj = {name: result.playlist.name, tracks: [...result.playlist.tracks].slice(0, 3)};
+      resultArr.push(obj);
+      this.setData({
+        topList: resultArr
+      })
+    }
+    
+    // 放在此处统一更新，会导致当前排行榜部分长期白屏， 白屏： 等待所有的请求结束才会更新页面
+    // this.setData({
+    //   topList: resultArr
+    // })
   
   },
 
