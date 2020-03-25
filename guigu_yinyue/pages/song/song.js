68440d0849a0f835426bea240d0631983419b0f7
zhiyongzaixian <13852292775@163.com>
2020-03-25 15:31:25 +0800

    'xx'

---
+++ guigu_yinyue/pages/song/song.js
@@ -0,0 +1,74 @@
+// pages/song/song.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isPlay: false, // 标识音乐是否播放， 默认为未播放
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  // 控制音乐播放
+  playControl(){
+    let isPlay = !this.data.isPlay;
+    
+    this.setData({
+      isPlay
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
