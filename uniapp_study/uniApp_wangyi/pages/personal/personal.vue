e90f3be0f10e814e34a0bbf8a5b797c79a426431
zhiyongzaixian <13852292775@163.com>
2020-03-28 12:02:00 +0800

    'xx'

--- uniapp_study/uniApp_wangyi/pages/personal/personal.vue
+++ uniapp_study/uniApp_wangyi/pages/personal/personal.vue
@@ -1,10 +1,13 @@
 <template>
 	<view>
+		<text>个人中心</text>
 		
+		<button type="primary" @click="getOpenId">获取用户唯一标识</button>
 	</view>
 </template>
 
 <script>
+	import request from '../../utils/request.js'
 	export default {
 		data() {
 			return {
@@ -12,8 +15,25 @@
 			}
 		},
 		methods: {
-			
-		}
+			getOpenId(){
+				// 1. 通过wx.login()获取登录凭证 code
+				wx.login({
+					success: async (res) => {
+						console.log(res);
+						let code = res.code;
+						// 2. 将code发送给小程序服务器
+						let result = await request('/getOpenId', {code})
+					}
+				})
+			}
+		},
+		mounted(){
+			// wx.getUserInfo({
+			// 	success: (res) => {
+			// 		console.log(res);
+			// 	}
+			// })
+		},
 	}
 </script>
 
--- uniapp_study/uniApp_wangyi/wangyi_study_server/server.js
+++ uniapp_study/uniApp_wangyi/wangyi_study_server/server.js
@@ -1,5 +1,9 @@
 let Koa = require('koa');
 let KoaRouter = require('koa-router');
+let jwt = require('jsonwebtoken');
+let Fly=require("flyio/src/node");
+let fly=new Fly;
+
 
 
 // 1. 生成 应用
@@ -18,12 +22,72 @@ router.get('/test', (ctx, next) => {
 });
 
 let cateNavList = require('./cateNavDatas.json')
+
+// 分类的接口
 router.get('/categoryNavList', (ctx, next) => {
 	// ctx： 代替req和res
 	
 	// koa中返回数据： ctx.body = value
 	ctx.body = cateNavList;
 });
+
+
+
+
+// 获取用户唯一标识openId的
+router.get('/getOpenId', async (ctx, next) => {
+	// 1) 获取请求参数
+	let code = ctx.query.code;
+	console.log('浏览器端携带的请求餐数据： code', code);
+	let appId = 'wx810e8b1fde386fde'
+	let appSecret =  '8dae7e93a191bbc11a4e9395d2901561'
+	
+	// 2) 发送请求给微信服务器获取用户唯一标识
+	let url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`;
+	
+	// 3) 发送请求
+	let result = await fly.get(url);
+	console.log('微信服务器返回的数据: ',  result.data);
+	  // .then(function (response) {
+	  //   console.log(response);
+	  // })
+	  // .catch(function (error) {
+	  //   console.log(error);
+	  // });
+	result = JSON.parse(result.data);
+	console.log(result);
+	console.log(typeof result);
+	
+	// 4) 自定义登录状态， 对openId进行加密，生成token， jsonwebtoken   jwt
+	let openId = result.openid;
+	// 5) jsonwebtoken对openid进行加密
+	// let token = jwt.sign(data, secret); 生成token， 
+	// let data = jwt.verify(token, secret); // 注意： 反编译token的密钥必须和当初生成token的密钥一致
+	
+	let token = jwt.sign(openId, 'jiajingwen');
+	
+	// 反编译token练习
+	// let test = jwt.verify(token); // 没有密钥： secret or public key must be provided 
+	// let test = jwt.verify(token, 'gaolaozhuang'); // 密钥错误：JsonWebTokenError: invalid signature
+	let test = jwt.verify(token, 'jiajingwen');
+	console.log('反编译token的结果： ', test); 
+	ctx.body = token;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 3. 声明使用路由
